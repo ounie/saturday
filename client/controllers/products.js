645bf5696ec71ae56bd21d0de82ea9d41a951a7f
Ounie <ounie@ounie.com>
2015-12-05 16:36:15 -0800

    ng-repeat example

--- client/controllers/products.js
+++ client/controllers/products.js
@@ -1,9 +1,42 @@
 angular.module('saturday').controller('ProductsCtrl',function ($scope) {
 
     $scope.product = 'Apple iPhone 2';
+    $scope.totalCost = 999;
 
-    $scope.price = function(){
-        return 199;
+    $scope.price = function(product){
+        if (product === 'iPhone'){
+            return 199;
+        }
+        else if (product === 'iPod'){
+            return 79;
+        }
+        else
+        {
+            return 29;
+        }
     }
+
+    var product1 = {
+        name: 'iPhone',
+        price: 299,
+        description: 'Best phone ever',
+        photo: 'images/iphone.jpg'
+    };
+
+    var product2 = {
+        name: 'iPod',
+        price: 79,
+        description: 'Music to my ears',
+        photo: 'images/ipod.jpg'
+    };
+
+    var product3 = {
+        name: 'iWatch',
+        price: 49,
+        description: 'Clock on my wrist',
+        photo: 'images/iwatch.jpg'
+    };
+
+    $scope.products = [product1, product2, product3];
 
 });
